55ecafab6db346da97c24813fe67cc6a4dcfb1d3
ByWizKi <enzoth39260@gmail.com>
2024-01-23 12:42:40 +0100

    tp2 terminer

--- TP2/tp2_demo_1.js
+++ TP2/tp2_demo_1.js
@@ -2,3 +2,5 @@
 console.info(`Le script ${document.currentScript.src} s'exécute...`);
 console.debug(document.querySelectorAll("a"));
 console.info(`...le script ${document.currentScript.src} a terminé.`);
+
+
